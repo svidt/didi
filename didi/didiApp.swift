59d6fd4eba1e55ee31ae0d088c5d0eff66ab0320
Emil <79602784+svidt@users.noreply.github.com>
2024-02-19 21:35:33 +0100

    Add files via upload

---
+++ didi/didiApp.swift
@@ -0,0 +1,17 @@
+//
+//  didiApp.swift
+//  didi
+//
+//  Created by Kristian Emil on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct didiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
